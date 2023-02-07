ddb9ef89e81f024868a030a1eee3d29757b3da7f
Maksim Moryakov <maksim.moryakov44@yandex.ru>
2023-02-07 17:48:29 +0300

    Создание админки

--- yatube/posts/admin.py
+++ yatube/posts/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+# Из модуля models импортируем модель Post
+from .models import Post
 
-# Register your models here.
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('text', 'pub_date', 'author') 
+    search_fields = ('text',) 
+    list_filter = ('pub_date',) 
+
+admin.site.register(Post, PostAdmin) 
