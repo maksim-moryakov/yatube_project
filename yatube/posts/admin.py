d94029ea3141afe2a95eb570b68d682921adfe12
Maksim Moryakov <maksim.moryakov44@yandex.ru>
2023-02-08 14:26:15 +0300

    настройка админки

--- yatube/posts/admin.py
+++ yatube/posts/admin.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
-# Из модуля models импортируем модель Post
 from .models import Post
 
 class PostAdmin(admin.ModelAdmin):
-    list_display = ('text', 'pub_date', 'author') 
+    list_display = ('pk', 'text', 'pub_date', 'author')
     search_fields = ('text',) 
-    list_filter = ('pub_date',) 
+    list_filter = ('pub_date',)
+    empty_value_display = '-пусто-'
 
 admin.site.register(Post, PostAdmin) 
